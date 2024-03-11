2bb8380bfc272cc399ad0e34683a6d3dc2891fcf
josephmj0303 <155250673+josephmj0303@users.noreply.github.com>
2024-03-11 08:27:28 +0530

    Update Filecontentscript.sh

--- Filecontentscript.sh
+++ Filecontentscript.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-echo "Hostname is 'hostname'"
-echo "Build is completed with webhook"
+# Print server name
+echo "Name: $(hostname)"
+
+# Print Memory details
+echo "Memory:"
+free -h
+
+# Print CPU details
+echo "CPU:"
+lscpu
+
+# Print Disk details
+echo "Disk:"
+df -h
